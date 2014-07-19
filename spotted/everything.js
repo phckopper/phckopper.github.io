c7fb57f90a06a3d82c0accd325f990cf223c0f7c
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2014-07-19 02:59:03 -0300

    1405749543

--- spotted/everything.js
+++ spotted/everything.js
@@ -2,4 +2,21 @@ var db = new Firebase('https://crackling-fire-6957.firebaseio.com/');
 db.on('child_added', function (snapshot) {
 	var post = snapshot.val();
 	$('#content').append('<div class="post">' + post.text + '<br><hr><span class="target">' + post.target + '</span></div><hr>');
-})
+});
+$('#text').keypress(function (e){
+	if (e.keyCode == 13){
+		sendPost();
+	}
+});
+$('#target').keypress(function (e){
+	if (e.keyCode == 13){
+		sendPost();
+	}
+});
+function sendPost(){
+	var text = $('#text').val();
+	var target = $('#target').val();
+	db.push({text: text, target: target});
+	$('#text').val('');
+	$('#target').val('');
+}
