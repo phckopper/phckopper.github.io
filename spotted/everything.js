0d4549949cda4827bd0b2dd764e2b34688532348
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2014-07-19 02:37:44 -0300

    1405748264

--- spotted/everything.js
+++ spotted/everything.js
@@ -1,5 +1,5 @@
 var db = new Firebase('https://crackling-fire-6957.firebaseio.com/');
 db.on('child_added', function (snapshot) {
 	var post = snapshot.val();
-	$('#content').append('<span class="post">' + post.text + '<br><span class="target">' + post.target + '</span>');
+	$('#content').append('<span class="post">' + post.text + '<br><hr><span class="target">' + post.target + '</span>');
 })
