2f842849ee937cf9eb804c64c6b043d48c38a765
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2016-06-13 00:55:36 -0300

    add download button

--- spotted/code.js
+++ spotted/code.js
@@ -54,3 +54,15 @@ function draw() {
 		ctx.fillText("M", canvas.width/2, 380);
 	};
 }
+
+function dlCanvas() {
+  var dt = canvas.toDataURL('image/png');
+  /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+  dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+  /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
+  dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
+
+  this.href = dt;
+};
+document.getElementById("dl").addEventListener('click', dlCanvas, false);
