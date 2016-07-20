7a41ac0ab4d215ed6cfc691276c0830196b46f5e
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2016-07-20 14:05:17 -0300

    i hate facebooks api

--- gv/main.js
+++ gv/main.js
@@ -1,5 +1,14 @@
 function eita() {
 	FB.login(function (auth) {
-		console.log(auth.userID);
+		FB.getLoginStatus(function(response) {
+		  if (response.status === 'connected') {
+		    // the user is logged in and has authenticated your
+		    // app, and response.authResponse supplies
+		    // the user's ID, a valid access token, a signed
+		    // request, and the time the access token 
+		    // and signed request each expire
+		    var uid = response.authResponse.userID;
+		    console.log(uid);
+		  }
 	})
 }
